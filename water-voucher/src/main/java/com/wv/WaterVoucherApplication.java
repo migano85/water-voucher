c68300b47332fd9392a5fc2535fda00e0d5ea8ec
migano85 <mahmoudyacine@yahoo.com>
2022-08-19 22:10:53 +0300

    1

---
+++ water-voucher/src/main/java/com/wv/WaterVoucherApplication.java
@@ -0,0 +1,13 @@
+package com.wv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterVoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterVoucherApplication.class, args);
+	}
+
+}
