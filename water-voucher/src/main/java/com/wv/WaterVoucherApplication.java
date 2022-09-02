466e4410e2e5614204cc10948a8b419c0de09583
migano85 <mahmoudyacine@yahoo.com>
2022-09-02 10:57:28 +0300

    calling DAO api

--- water-voucher/src/main/java/com/wv/WaterVoucherApplication.java
+++ water-voucher/src/main/java/com/wv/WaterVoucherApplication.java
@@ -1,13 +1,32 @@
 package com.wv;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WaterVoucherApplication {
+import com.wv.repositories.CustomerRepoJdbcImpl;
 
+@SpringBootApplication
+public class WaterVoucherApplication implements CommandLineRunner{
+
+	private static final Logger log = LoggerFactory.getLogger(WaterVoucherApplication.class);
+	
+	@Autowired
+	private CustomerRepoJdbcImpl customerRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WaterVoucherApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("***********");
+		log.info(customerRepo.count() + "");
+		log.info("***********");
+		
 	}
 
 }
--- water-voucher/src/main/java/com/wv/model/Bottle.java
+++ water-voucher/src/main/java/com/wv/model/Bottle.java
@@ -10,10 +10,15 @@ import lombok.ToString;
 @ToString(doNotUseGetters = true)
 public class Bottle {
 
+	/******************
+	* as the case of book-voucher aggregate, bottle never exist outside the contest of voucher
+	* so voucher will be aggregate root and bottle will not have mapping to voucher (one-directional relationship)
+	******************
+	*/
 	private Long bottleId;
 	private Double size; //in little
 	private String serialNo;
-	private boolean full;
+	private boolean filled;
 	//private List<OrderBottle> bottleOrdersList; //(History:maximum 1 has deliveryDate=null)
 	private Date createdAt;
 	private Date modifiedAt; //(changed automatically on every record change) will be used for optimistic locking
