c53edaf7b22d2888ff13cf3f2b725b81b9f7b2c4
migano85 <mahmoudyacine@yahoo.com>
2022-10-21 19:42:38 +0300

    .

--- water-voucher/src/main/java/com/wv/testing/TestStreams.java
+++ water-voucher/src/main/java/com/wv/testing/TestStreams.java
@@ -1,50 +1,50 @@
-package com.wv.testing;
-
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
-
-public class TestStreams {
-
-	public static boolean isLongerThan4(String word) {
-		return word != null &&  word.length()>4;
-	}
-	
-	public static void main(String[] args) {
-		
-		Predicate<String> biggerThan4Predicate = TestStreams::isLongerThan4;
-		/*supplier is added only to use the Stream more than once 
-		*and avoid the exception: stream has already been operated upon or closed
-		*what supplier does is it create new Stream every time get() is called
-		*Supplier<T> is functional interface that can be applied to anything.
-		*
-		*   Example:
-		*   -------
-		*   Supplier<String> strSupplier = ()->"ee";
-		*   System.out.println(strSupplier.get());
-		*/
-		Supplier<Stream<String>> wordsStreamSupplier = () -> Stream.of("nano", "bingo", "mahmoud", "lola", "go","mourad");
-		Supplier<Stream<String>> big4StreamSuppier = ()->wordsStreamSupplier.get().filter(biggerThan4Predicate);
-		Supplier<Stream<String>> big5StreamSuppier = ()->wordsStreamSupplier.get().filter(s-> s.length() > 5);
-		
-		boolean allMatch = big4StreamSuppier.get().allMatch(w -> w.length()>4);
-		System.out.println(allMatch);
-		System.out.println("%%%%%%%%%%%%%%%%%%%%%");
-		System.out.println(big4StreamSuppier.get().toList());
-		System.out.println("*********************");
-		wordsStreamSupplier.get().forEach(w->System.out.println(w));
-		System.out.println("********************");
-		Consumer<String> consumerStr = s->{System.out.print(s); System.out.print(" - ");}; //accept method of consumer just takes one argument execute some code and return nothing
-		big5StreamSuppier.get().forEach(consumerStr);
-		System.out.println("%%%%%%%%%%%%%%%%%%%%%");
-		
-		big5StreamSuppier.get().map(null);
-		//------------------------------------------
-		Supplier<String> strSupplier = ()->"ee";
-		System.out.println(strSupplier.get());
-		
-		
-	}
-
-}
+//package com.wv.testing;
+//
+//import java.util.function.Consumer;
+//import java.util.function.Predicate;
+//import java.util.function.Supplier;
+//import java.util.stream.Stream;
+//
+//public class TestStreams {
+//
+//	public static boolean isLongerThan4(String word) {
+//		return word != null &&  word.length()>4;
+//	}
+//	
+//	public static void main(String[] args) {
+//		
+//		Predicate<String> biggerThan4Predicate = TestStreams::isLongerThan4;
+//		/*supplier is added only to use the Stream more than once 
+//		*and avoid the exception: stream has already been operated upon or closed
+//		*what supplier does is it create new Stream every time get() is called
+//		*Supplier<T> is functional interface that can be applied to anything.
+//		*
+//		*   Example:
+//		*   -------
+//		*   Supplier<String> strSupplier = ()->"ee";
+//		*   System.out.println(strSupplier.get());
+//		*/
+//		Supplier<Stream<String>> wordsStreamSupplier = () -> Stream.of("nano", "bingo", "mahmoud", "lola", "go","mourad");
+//		Supplier<Stream<String>> big4StreamSuppier = ()->wordsStreamSupplier.get().filter(biggerThan4Predicate);
+//		Supplier<Stream<String>> big5StreamSuppier = ()->wordsStreamSupplier.get().filter(s-> s.length() > 5);
+//		
+//		boolean allMatch = big4StreamSuppier.get().allMatch(w -> w.length()>4);
+//		System.out.println(allMatch);
+//		System.out.println("%%%%%%%%%%%%%%%%%%%%%");
+//		System.out.println(big4StreamSuppier.get().toList());
+//		System.out.println("*********************");
+//		wordsStreamSupplier.get().forEach(w->System.out.println(w));
+//		System.out.println("********************");
+//		Consumer<String> consumerStr = s->{System.out.print(s); System.out.print(" - ");}; //accept method of consumer just takes one argument execute some code and return nothing
+//		big5StreamSuppier.get().forEach(consumerStr);
+//		System.out.println("%%%%%%%%%%%%%%%%%%%%%");
+//		
+//		big5StreamSuppier.get().map(null);
+//		//------------------------------------------
+//		Supplier<String> strSupplier = ()->"ee";
+//		System.out.println(strSupplier.get());
+//		
+//		
+//	}
+//
+//}
--- water-voucher/src/main/java/com/wv/testing/TestStreams2.java
+++ water-voucher/src/main/java/com/wv/testing/TestStreams2.java
@@ -1,94 +1,94 @@
-package com.wv.testing;
-
-import java.util.Optional;
-import java.util.stream.Stream;
-
-public class TestStreams2 {
-
-	public static void main(String[] args) {
-		//*************RULE of thumb***********
-		//intermediate stream operations are lazy, will not work until you provide a terminate method
-		//check https://www.baeldung.com/java-streams-peek-api for more informatu
-		//**************************
-		//peek()‘s Javadoc page says: “This method exists mainly to support debugging, where you want to see the elements as they flow past a certain point in a pipeline“. 
-		//
-		//On top of that, peek() can be useful in another scenario: when we want to alter the inner state of an element. For example, let's say we want to convert all user's name to lowercase before printing them:
-		//
-		//Stream<User> userStream = Stream.of(new User("Alice"), new User("Bob"), new User("Chuck"));
-		//userStream.peek(u -> u.setName(u.getName().toLowerCase()))
-		//  .forEach(System.out::println);
-		//Alternatively, we could have used map(), but peek() is more convenient since we don't want to replace the element.
-		//
-		//**************************************************
-//		Stream.iterate(1, n -> n + 2)
-//		.peek(x -> System.out.println("peeking " + x))  it will not modify the data of the stream like map
-//        .limit(5)
-//        .peek(x -> System.out.println("peeking again " + x)) peek creates new stream copy of the original, that's why it will not change data unless they are reference Object, like the above example
-//        .forEach(x -> System.out.println(x));
-		
-//		Stream.iterate("mahmoud", n->"mourad")
-//		.peek(n->System.out.println(n.toUpperCase()))//peek does not transform data in stream
-//		.limit(3)
-//		.forEach(System.out::println);//this print lower case because peek did not transform data
-		
-//		Stream.iterate("mahmoud", n->"mourad")
-//		.map(String::toUpperCase())//map transform data in stream, here we use the class to calls instance methods (toUpper not static method)
-//		.limit(4)
-//		.forEach(System.out::println);//this will print upper case
-//		String str = "gigo"; str.toUpperCase();
-		//--------------------------------------------
-//		Stream.of(3,6,7,9)
-//		.map(Integer::doubleValue)//map transform data in stream, here we use the class to calls instance methods (doubleValue not static method)
-//		.limit(4)
-//		.map(Double::longValue)//because first map converted integer to double, now we have to call methods of double instance
-//		.forEach(System.out::println);//this will print upper case
-		
-//		Integer x = 5; x.doubleValue();
-		
-		//*************************************************************
-//		Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
-//		.limit(10)
-//		//.map(n -> n[0]) //transform array element value to value
-//		.forEach(x -> {System.out.print(x[0]); System.out.print(" - "); System.out.println(x[1]);});
-		//*************************************************************
-		
-//		Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
-//		.limit(10)
-//		.map(n -> n[0]) //transform array element value to value
-//		.forEach(System.out::println); //forEach takes a consumer then it execute its accept() method which we give implementation as println
-		
-		//this is will do the same as above but with 2 steps
-//		Stream<Integer> s = Stream.iterate(new int[]{0, 1, 70}, n -> new int[]{n[1], n[0] + n[1]})
-//		.limit(10)
-//		.map(n -> n[0]);
+//package com.wv.testing;
+//
+//import java.util.Optional;
+//import java.util.stream.Stream;
+//
+//public class TestStreams2 {
+//
+//	public static void main(String[] args) {
+//		//*************RULE of thumb***********
+//		//intermediate stream operations are lazy, will not work until you provide a terminate method
+//		//check https://www.baeldung.com/java-streams-peek-api for more informatu
+//		//**************************
+//		//peek()‘s Javadoc page says: “This method exists mainly to support debugging, where you want to see the elements as they flow past a certain point in a pipeline“. 
+//		//
+//		//On top of that, peek() can be useful in another scenario: when we want to alter the inner state of an element. For example, let's say we want to convert all user's name to lowercase before printing them:
+//		//
+//		//Stream<User> userStream = Stream.of(new User("Alice"), new User("Bob"), new User("Chuck"));
+//		//userStream.peek(u -> u.setName(u.getName().toLowerCase()))
+//		//  .forEach(System.out::println);
+//		//Alternatively, we could have used map(), but peek() is more convenient since we don't want to replace the element.
+//		//
+//		//**************************************************
+////		Stream.iterate(1, n -> n + 2)
+////		.peek(x -> System.out.println("peeking " + x))  it will not modify the data of the stream like map
+////        .limit(5)
+////        .peek(x -> System.out.println("peeking again " + x)) peek creates new stream copy of the original, that's why it will not change data unless they are reference Object, like the above example
+////        .forEach(x -> System.out.println(x));
 //		
-//		for(Integer n : s.toList()) {
-//			System.out.println(n);
-//		}
-		
-		//________________________________________________________
-//		double sum = Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
-//		.limit(10)
-//		.map(n -> n[0]) //transform array element value to value, transform the result from Stream<int[]> to Stream<Integer>
-//		.mapToDouble(n->n)
-//		.sum();
+////		Stream.iterate("mahmoud", n->"mourad")
+////		.peek(n->System.out.println(n.toUpperCase()))//peek does not transform data in stream
+////		.limit(3)
+////		.forEach(System.out::println);//this print lower case because peek did not transform data
 //		
-//		System.out.println(sum);
-		
-		//%%%%%%%% JAVA 9 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-		// The stream.iterate was enhanced in Java 9. It supports a predicate (condition) as second argument, and the stream.iterate will stop if the predicate is false.
-//		Stream.iterate(1, n -> n < 10 , n -> n * 2)
-//		.limit(3)
-//        .forEach(x -> System.out.println(x));
-		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-		
-//		Integer x = Stream.iterate(4, multipleOfFour -> multipleOfFour + 4)
-//		.limit(10)
-//		.map(multipleOfFour -> multipleOfFour * multipleOfFour)
-//		.filter(multipleOfFourSquared -> multipleOfFourSquared % 10 == 0)
-//		.reduce(0, Integer::sum);
+////		Stream.iterate("mahmoud", n->"mourad")
+////		.map(String::toUpperCase())//map transform data in stream, here we use the class to calls instance methods (toUpper not static method)
+////		.limit(4)
+////		.forEach(System.out::println);//this will print upper case
+////		String str = "gigo"; str.toUpperCase();
+//		//--------------------------------------------
+////		Stream.of(3,6,7,9)
+////		.map(Integer::doubleValue)//map transform data in stream, here we use the class to calls instance methods (doubleValue not static method)
+////		.limit(4)
+////		.map(Double::longValue)//because first map converted integer to double, now we have to call methods of double instance
+////		.forEach(System.out::println);//this will print upper case
 //		
-//		System.out.println(x);
-	}
-
-}
+////		Integer x = 5; x.doubleValue();
+//		
+//		//*************************************************************
+////		Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+////		.limit(10)
+////		//.map(n -> n[0]) //transform array element value to value
+////		.forEach(x -> {System.out.print(x[0]); System.out.print(" - "); System.out.println(x[1]);});
+//		//*************************************************************
+//		
+////		Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+////		.limit(10)
+////		.map(n -> n[0]) //transform array element value to value
+////		.forEach(System.out::println); //forEach takes a consumer then it execute its accept() method which we give implementation as println
+//		
+//		//this is will do the same as above but with 2 steps
+////		Stream<Integer> s = Stream.iterate(new int[]{0, 1, 70}, n -> new int[]{n[1], n[0] + n[1]})
+////		.limit(10)
+////		.map(n -> n[0]);
+////		
+////		for(Integer n : s.toList()) {
+////			System.out.println(n);
+////		}
+//		
+//		//________________________________________________________
+////		double sum = Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+////		.limit(10)
+////		.map(n -> n[0]) //transform array element value to value, transform the result from Stream<int[]> to Stream<Integer>
+////		.mapToDouble(n->n)
+////		.sum();
+////		
+////		System.out.println(sum);
+//		
+//		//%%%%%%%% JAVA 9 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//		// The stream.iterate was enhanced in Java 9. It supports a predicate (condition) as second argument, and the stream.iterate will stop if the predicate is false.
+////		Stream.iterate(1, n -> n < 10 , n -> n * 2)
+////		.limit(3)
+////        .forEach(x -> System.out.println(x));
+//		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//		
+////		Integer x = Stream.iterate(4, multipleOfFour -> multipleOfFour + 4)
+////		.limit(10)
+////		.map(multipleOfFour -> multipleOfFour * multipleOfFour)
+////		.filter(multipleOfFourSquared -> multipleOfFourSquared % 10 == 0)
+////		.reduce(0, Integer::sum);
+////		
+////		System.out.println(x);
+//	}
+//
+//}
