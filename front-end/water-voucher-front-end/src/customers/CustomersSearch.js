63eea7a16d33cb392b0e89704cafd1ad4ebd719b
mahmoud yasine <mahmoudyacine@yahoo.com>
2023-01-01 14:54:26 +0300

    .

--- front-end/water-voucher-front-end/src/customers/CustomersSearch.js
+++ front-end/water-voucher-front-end/src/customers/CustomersSearch.js
@@ -13,25 +13,35 @@ const CustomersSearch = () => {
   const hanldeSubmit = (e) => {
     e.preventDefault();
     const customerCriteria = { firstName, lastName };
-
+    const abortCont = new AbortController();
+    
     fetch("http://localhost:8081/customers/search", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(customerCriteria),
+      signal: abortCont.signal
     })
     //---------- method 1 using return promise -------------------
     .then((res) => {
       if (!res.ok) {
-        alert("something went wrong");
+        throw Error("could not fetch the data for that resource");
       }
       return res.json();
     }).then((data) => {
         setCustomers(data);
+      })
+      .catch((err) => {
+        // auto catches network / connection error
+        if (err !== "AbortError") {
+          //if abortCont.abort() is executed it will return an error, which means that the component is unmounted, in the that case we do not want to update the state of IsPending and error
+          // setIsPending(false);
+          // setError(err.message);
+        }
       });
     //----------- method 2 using async .. await -----------------
     /*.then(async (res) => {
       if (!res.ok) {
-        alert("something went wrong");
+        throw Error("could not fetch the data for that resource");
       }
       setCustomers(await res.json());
     });*/
