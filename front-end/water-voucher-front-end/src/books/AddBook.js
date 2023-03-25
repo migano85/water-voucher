32bc31c167bf7f6f5d4f76fce1ed6d4d95e8d2d9
mahmoud yasine <mahmoudyacine@yahoo.com>
2023-03-25 14:15:28 +0300

    complete addBook

--- front-end/water-voucher-front-end/src/books/AddBook.js
+++ front-end/water-voucher-front-end/src/books/AddBook.js
@@ -1,31 +1,46 @@
 import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import useFetch from "../useFetch";
 import Select from "react-select";
 import "./AddBook.css";
 
 const AddBook = () => {
-  const [selectedOptions, setSelectedOptions] = useState();
+  const history = useHistory();
+  const [selectedOption, selectedOptionCallback] = useState();
+  const [numberOfPages, setNoOfPagesValue] = useState(0);
+  const [customerId, setCustomerId] = useState(0);
 
   const { error, isPending, resData } = useFetch(
     "http://localhost:8080/customers/all"
   );
 
-  const [optionList, setOptionList] = useState([]);
-  console.log(resData);
-  // setOptionList(resData);
-
-  // const optionList = [
-  //   { value: "red", label: "Red" },
-  //   { value: "green", label: "Green" },
-  //   { value: "yellow", label: "Yellow" },
-  //   { value: "blue", label: "Blue" },
-  //   { value: "white", label: "White" },
-  // ];
-
   // Function triggered on selection
-  function handleSelect(data) {
-    setSelectedOptions(data);
+  function handleSelect(selectedValue) {
+    selectedOptionCallback(selectedValue);
+    setCustomerId(selectedValue.customerId);
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const book = {
+      numberOfPages,
+      customerId,
+    };
+    console.log(book);
+    fetch("http://localhost:8080/book-api/books", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(book),
+    }).then((res) => {
+      if (!res.ok) {
+        alert("something went wrong");
+        console.log(res);
+      } else {
+        document.querySelector("button").disabled = true;
+        history.push("/");
+      }
+    });
+  };
 
   const customStyles = {
     control: (provided, state) => ({
@@ -58,7 +73,7 @@ const AddBook = () => {
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <table>
           <tbody>
             <tr>
@@ -66,28 +81,41 @@ const AddBook = () => {
                 <label>No of pages</label>
               </td>
               <td>
-                <input id="txtPagesNo" type="text" required />
+                <input
+                  id="txtPagesNo"
+                  type="text"
+                  placeholder="enter number of pages"
+                  onChange={(e) => setNoOfPagesValue(e.target.value)}
+                  required
+                />
               </td>
             </tr>
             <tr>
               <td>
-                <label>customer</label>
+                <label>
+                  customer {selectedOption != null && selectedOption.firstName}
+                </label>
               </td>
               <td className="dropdown-container">
                 <Select
                   options={resData}
-                  getOptionLabel={(e) => e.firstName}
-                  getOptionValue={(e) => e.customerId}
+                  getOptionLabel={(e) => e.firstName + " " + e.lastName}
+                  getOptionValue={(e) => e}
                   placeholder="Select customer"
-                  value={selectedOptions}
+                  value={selectedOption}
                   onChange={handleSelect}
                   isSearchable={true}
                   styles={customStyles}
+                  // isMulti
                 />
               </td>
             </tr>
           </tbody>
         </table>
+        <button>Add Book</button>
+        <Link className="linkAsButton" to="/">
+          Customers
+        </Link>
       </form>
     </div>
   );
