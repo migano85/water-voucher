0e8469e55dd968b9dbbd09eba6e37f2c81ba6bef
mahmoud yasine <mahmoudyacine@yahoo.com>
2023-03-26 15:39:18 +0300

    vouchers part 1

--- water-voucher/src/main/java/com/wv/model/Voucher.java
+++ water-voucher/src/main/java/com/wv/model/Voucher.java
@@ -2,7 +2,12 @@ package com.wv.model;
 
 import java.util.Date;
 
+import com.wv.jooq.model.tables.pojos.Vouchers;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 /*
@@ -10,16 +15,13 @@ import lombok.ToString;
  * which means book class needs Service class that will generate random serials that will be concatinated to book No and stored as voucher serials
  * user can only delete voucher inside a book
  */
-@Data
-@ToString(doNotUseGetters = true)
-public class Voucher {
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper=true)
+@ToString(doNotUseGetters = true, callSuper = true)
+public class Voucher extends Vouchers {
 
-	private Long voucherId;
-	private Long voucherSerial;
 	private Bottle bottle;//one to one
 //	private Book book; //voucher never exist outside a book, so it is part of an aggregate where book is the aggregate root, so it will be one direction relationship withBook having reference to voucher and not vice-versa
-	private Date createdAt;
-	private Long createBy;
-	private Date modifiedAt; //(changed automatically on every record change) will be used for optimistic locking
-	private Long modifyBy;
+
 }
--- water-voucher/src/main/java/com/wv/services/BookService.java
+++ water-voucher/src/main/java/com/wv/services/BookService.java
@@ -1,9 +1,15 @@
 package com.wv.services;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wv.model.Book;
+import com.wv.model.Voucher;
 import com.wv.repositories.BookRepoJooqImpl;
 
 @Service
@@ -14,8 +20,23 @@ public class BookService {
     
     public void createBookWithVouchers(Book book){
         repoJooqImpl.save(book);
-        //loop on number of pages using java 8 code and insert voucher records accordinglly
-        //or use jooq to insert list of vouchers at same time
-        book.getNumberOfPages();
+
+        ArrayList<Voucher> vouchersList = new ArrayList<>();
+        for(int i =0; i < book.getNumberOfPages(); i++){
+            Voucher voucher = new Voucher();
+            voucher.setVoucherSerial(createVoucherSerial());
+            voucher.setBookId(book.getBookId());
+            vouchersList.add(voucher);
+        }
+
+        repoJooqImpl.fillBookVouchers(book.getBookId(), vouchersList);
+    }
+
+    public static String createVoucherSerial(){
+        return Calendar.getInstance().get(Calendar.YEAR) +  
+         Calendar.getInstance().get(Calendar.MONTH) + 
+         Calendar.getInstance().get(Calendar.DATE) + 
+         Calendar.getInstance().getTimeInMillis() + 
+         StringUtils.leftPad((new Random().nextInt(10000)+""), 5, "0");
     }
 }
